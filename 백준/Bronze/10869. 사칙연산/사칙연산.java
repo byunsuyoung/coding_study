b8606451d10cbae9a9e980912cc3f0e3c283e7d4
byunsuyoung <116817402+byunsuyoung@users.noreply.github.com>
2023-03-29 17:54:27 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,17 +1,20 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		scan.close();
+		String[] str = br.readLine().split(" ");
+		int a = Integer.parseInt(str[0]);
+		int b = Integer.parseInt(str[1]);
 		
 		System.out.println(a+b);
 		System.out.println(a-b);
 		System.out.println(a*b);
 		System.out.println(a/b);
 		System.out.println(a%b);
-		}
+ 
+	}
 }
