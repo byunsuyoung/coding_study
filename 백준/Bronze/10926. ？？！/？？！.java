84faa4c05ef6d75d53a0af03adda253aefcfaf72
byunsuyoung <116817402+byunsuyoung@users.noreply.github.com>
2023-03-29 17:59:20 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		String a = scan.next();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		String a = br.readLine();
 		System.out.println(a + "??!");
-		}
+	}
 }
