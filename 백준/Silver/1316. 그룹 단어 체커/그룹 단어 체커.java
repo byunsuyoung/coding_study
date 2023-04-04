1310ca5f8b74be3262fcbbf686ee96dd5891808b
byunsuyoung <116817402+byunsuyoung@users.noreply.github.com>
2023-04-04 19:57:12 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		int count = 0;
+		int N = Integer.parseInt(br.readLine());
+ 
+		for (int i = 0; i < N; i++) {
+			if (check()) {
+				count++;
+			}
+		}
+		System.out.print(count);
+	}
+ 
+	public static boolean check() throws IOException {
+		boolean[] check = new boolean[26];
+		int prev = 0;
+		String str = br.readLine();
+		
+		for(int i = 0; i < str.length(); i++) {
+			int now = str.charAt(i);
+			
+			
+			if (prev != now) {	//해당 문자가 처음나오는 경우	
+				if (!check[now - 'a']) {
+					check[now - 'a'] = true;
+					prev = now;	
+				}
+				else {
+					return false;	
+				}
+			}
+		}  //연속된 문자  
+		return true;
+	}
+}
