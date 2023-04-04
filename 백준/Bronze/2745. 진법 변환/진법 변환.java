12f1b3cf2ab967a9e313ba23a46c5d41197d8c97
byunsuyoung <116817402+byunsuyoung@users.noreply.github.com>
2023-04-04 20:43:17 +0900

    [Bronze II] Title: 진법 변환, Time: 212 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+        int b = scanner.nextInt();
+
+        System.out.println(Integer.parseInt(a,b));
+    }
+}
