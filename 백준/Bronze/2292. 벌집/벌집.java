be1403594a4691baf7c827dc9f36f10e0a7db67b
Byun suyoung <116817402+byunsuyoung@users.noreply.github.com>
2023-04-07 22:35:06 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int n = Integer.parseInt(br.readLine());
+		int count = 1; // 겹 수(최소 루트)
+		int range = 2;	// 범위 (최솟값 기준) 
+ 
+		if (n == 1) {
+			System.out.print(1);
+		}
+ 
+		else {
+			while (range <= n) { 
+				range = range + (6 * count);
+				count++;
+			}
+			System.out.print(count);
+		}
+	}
+}
