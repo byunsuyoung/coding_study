6818db492358713aa0ce55f32cb5992f4f0553a4
Byun suyoung <116817402+byunsuyoung@users.noreply.github.com>
2023-04-07 22:47:11 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13888 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,9 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+ 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("|\\_/|");
-		System.out.println("|q p|   /}");
-		System.out.println("( 0 )\"\"\"\\");
-		System.out.println("|\"^\"`    |");
-		System.out.println("||_/=\\\\__|");                     
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("|\\_/|\n");
+		bw.write("|q p|   /}\n");
+		bw.write("( 0 )\"\"\"\\\n");
+		bw.write("|\"^\"`    |\n");
+		bw.write("||_/=\\\\__|"); 
+		        
+		bw.flush(); //현재 버퍼에 저장되어 있는 내용을 클라이언트로 전송하고 버퍼를 비운다.
+		bw.close();
+	}
 }
