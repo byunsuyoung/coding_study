563a397c424d4776e87c63b6779cf711696144b6
Byun suyoung <116817402+byunsuyoung@users.noreply.github.com>
2023-04-11 17:37:29 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 224 ms, Memory: 14916 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		final int N = Integer.parseInt(br.readLine());
 		br.close();
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= i; j++) {
