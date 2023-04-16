c3d9abf7faa35272439ff84d609474c6bc7938a6
Byun suyoung <116817402+byunsuyoung@users.noreply.github.com>
2023-04-16 23:02:16 +0900

    [Bronze V] Title: 사칙연산, Time: 136 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.ts
+++ 백준/Bronze/10869. 사칙연산/사칙연산.ts
@@ -7,5 +7,7 @@ const b = parseInt(inputData[1]);
 console.log(a+b);
 console.log(a-b);
 console.log(a*b);
+// 몫을 구하는 연산자가 따로 존재하지 않아서
+// console.log(parseInt(a/b));  - 이것도 가능하다. 
 console.log(Math.floor(a/b)); 
 console.log(a%b);
