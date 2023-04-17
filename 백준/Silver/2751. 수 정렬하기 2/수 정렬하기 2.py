b123dbed4e4a06171df6892a9b2866ab61f62462
Byun suyoung <116817402+byunsuyoung@users.noreply.github.com>
2023-04-17 18:35:32 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1224 ms, Memory: 84924 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -2,6 +2,7 @@ import sys
 n = int(input())
 list = []
 for i in range(n):
-    list.append(int(sys.stdin.readline()))
+    list.append(int(sys.stdin.readline())) # str형으로 받아지므로 정수로 사용할 때 형 변환에 유의
+                                           # 개행 문자까지 포함한 하나의 줄을 한 번에 버퍼로 입력받는다.
 for i in sorted(list):
     sys.stdout.write(str(i)+'\n')
