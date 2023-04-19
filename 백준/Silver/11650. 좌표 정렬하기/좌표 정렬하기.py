66ccec67d000b2ca530ab03dff39368f2211ccca
Byun suyoung <116817402+byunsuyoung@users.noreply.github.com>
2023-04-19 20:53:46 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 404 ms, Memory: 54084 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+
+list = []
+for _ in range(N): 
+    x, y = map(int,input().split())
+    list.append([x,y])
+
+list.sort(key = lambda x: (x[0], x[1])) # key인자에 함수 넣으면 우선순위 지정됨
+
+for i in range(N): 
+    print(list[i][0], list[i][1])
