fd10a542978911e255ad56e59083dd7c39e88862
Byun suyoung <116817402+byunsuyoung@users.noreply.github.com>
2023-05-01 22:25:42 +0900

    [Silver V] Title: 최소공배수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.py
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -1,4 +1,15 @@
-import math
+import sys
+input = sys.stdin.readline
+
+def gcd_r(a,b) :
+    if b == 0 :
+        return a
+    else :
+        return gcd_r(b,a%b)
+
+def lcm(a,b) :
+    return a*b//gcd_r(a,b)
 
 a,b = map(int,input().split())
-print(a*b//math.gcd(a,b))
+
+print(lcm(a,b))
