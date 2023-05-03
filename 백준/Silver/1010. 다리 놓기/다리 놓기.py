39b1cc641f6d160913295cf906f915c843b7bd7d
Byun suyoung <116817402+byunsuyoung@users.noreply.github.com>
2023-05-03 21:49:39 +0900

    [Silver V] Title: 다리 놓기, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,13 @@
+def fact(n):
+    num = 1
+    for i in range(1, n+1):
+        num *= i
+    return num
+
+
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    result = fact(m) // (fact(n) * fact(m - n))
+    print(result)
